fd333eff2cfac92e3ade35a15c11c16c52d252a8
Ksawery Margas <ksawerymargas@gmail.com>
2026-06-01 22:53:55 +0200

    Update Form1.cs

--- KalkulatorKCAL/Form1.cs
+++ KalkulatorKCAL/Form1.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic;
+using System.Reflection;
+
 namespace KalkulatorKCAL
 {
     public partial class Form1 : Form
@@ -7,24 +10,38 @@ namespace KalkulatorKCAL
             InitializeComponent();
         }
 
+        private void DodajProdukt(ListBox lista)
+        {
+            Product p = new Product();
+
+            p.Name = Interaction.InputBox("Nazwa produktu:");
+            p.Calories = Convert.ToDouble(Interaction.InputBox("Kcal:"));
+            p.Protein = Convert.ToDouble(Interaction.InputBox("Białko:"));
+            p.Fat = Convert.ToDouble(Interaction.InputBox("Tłuszcz:"));
+            p.Carbs = Convert.ToDouble(Interaction.InputBox("Węglowodany:"));
+
+            lista.Items.Add(p);
+        }
+
         private void btnbreakfast_Click(object sender, EventArgs e)
         {
-
+            DodajProdukt(breakfast);
         }
 
         private void btnlunch_Click(object sender, EventArgs e)
         {
-
+            DodajProdukt(lunch);
         }
 
         private void btnsnack_Click(object sender, EventArgs e)
         {
-
+            DodajProdukt(snack);
         }
 
         private void btndinner_Click(object sender, EventArgs e)
         {
-
+            DodajProdukt(dinner);
         }
     }
 }
+
